6c351c0406843c9b91d8a5c61073891cf402b0ef
Rachel Simone Weil <partytimehexcellent@gmail.com>
2017-05-07 06:08:24 -0500

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,12 @@
 const socket = io();
-let targetimgurl = './public/img/1.jpg';
+let imgarray = [
+    'https://hxlntblob.blob.core.windows.net/strikethispose/scoha1.jpg',
+    'https://hxlntblob.blob.core.windows.net/strikethispose/scoha2.jpg',
+    'https://hxlntblob.blob.core.windows.net/strikethispose/scoha3.jpg',
+    'https://hxlntblob.blob.core.windows.net/strikethispose/scoha4.jpg',
+    'https://hxlntblob.blob.core.windows.net/strikethispose/scoha6.jpg',
+]
+let targetimgurl = imgarray[0];
 let targetFace = [];
 let targetEmotion = [];
 let incomingfaceurl = '';
@@ -32,12 +39,14 @@ socket.on('targetemotionresults', function (response) {
 socket.on('incomingemotionresults', function (response) {
     if (response == '') {
         alert("Oops! I couldn't find your face. Try again?");
+        videoinit();
     }
     else { incomingEmotion = response; }
 });
 socket.on('incomingfaceresults', function (response) {
     if (response == '') {
         alert("Oops! I couldn't find your face. Try again?");
+        videoinit();
     }
     else { incomingFace = response; }
 });
@@ -49,8 +58,35 @@ socket.on('incomingallresults', function () {
     $('#score').css( "opacity", "1" );
 });
 
-$('body').on('click', "#placeholder", function () {
-    document.getElementById('videocontainer').innerHTML = '<div class="flex-item" style="overflow:hidden;display:block;"><video id="video" autoplay style="transform: scaleX(-1);"></video><canvas id="canvas"></canvas></div><div class="flex-item" style="background-image:url(./img/1.jpg); transform: scaleX(-1);background-size: cover;"></div> <div class="flex-caption">Use your face...</div><div class="flex-caption">...to match this face!</div>'
+// When gray square is clicked, initialize video
+$('body').on('click', "#placeholder", videoinit);
+
+// When retry button is clicked, reset video
+$('body').on('click', '#retry', function(){
+    $('.buttoncontainer').html('<button id="snap">Snap photo</button>');
+    videoinit();
+});
+
+// When Snap photo button is clicked, draw video to canvas and send image to Cognitive Services
+$('body').on('click', '#snap', function () {
+    let video = document.getElementById('video');
+    let videow = $('#video').width();
+    document.getElementById('videocontainer').innerHTML = '<div class="flex-item" id="placeholder" style="overflow:hidden; display:block;"><canvas id="canvas" style="transform: scaleX(-1); padding:0;"></canvas></div><div class="flex-item" style="background-image:url(' + targetimgurl + '); transform: scaleX(-1);background-size: cover;"></div> <div class="flex-caption">Use your face...</div><div class="flex-caption">...to match this face!</div>'
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+    canvas.height = $('.flex-item').height();
+    canvas.width = $('.flex-item').width();
+    let offset = parseInt(canvas.width) - parseInt(videow);
+    context.drawImage(video, offset, 0);
+    incomingfaceurl = canvas.toDataURL("image/jpg", 0.7);
+    socket.emit('incomingface', incomingfaceurl);
+    socket.emit('incomingemotion', incomingfaceurl);
+});
+
+function videoinit () {
+    $('#badge').css( "opacity", "0" );
+    $('#score').css( "opacity", "0" );
+    document.getElementById('videocontainer').innerHTML = '<div class="flex-item" style="overflow:hidden;display:block;"><video id="video" autoplay style="transform: scaleX(-1);"></video><canvas id="canvas"></canvas></div><div class="flex-item" style="background-image:url(' + targetimgurl + '); transform: scaleX(-1);background-size: cover;"></div> <div class="flex-caption">Use your face...</div><div class="flex-caption">...to match this face!</div>'
     let video = document.getElementById('video');
     let videoh = $('.flex-item').height();
     let videow = $('.flex-item').width();
@@ -63,22 +99,7 @@ $('body').on('click', "#placeholder", function () {
             $('.buttoncontainer').css( "display", "block" );
         });
     }
-});
-
-$('body').on('click', '#snap', function () {
-    let video = document.getElementById('video');
-    let videow = $('#video').width();
-    document.getElementById('videocontainer').innerHTML = '<div class="flex-item" id="placeholder" style="overflow:hidden; display:block;"><canvas id="canvas" style="transform: scaleX(-1); padding:0;"></canvas></div><div class="flex-item" style="background-image:url(./img/1.jpg); transform: scaleX(-1);background-size: cover;"></div> <div class="flex-caption">Use your face...</div><div class="flex-caption">...to match this face!</div>'
-    var canvas = document.getElementById('canvas');
-    var context = canvas.getContext('2d');
-    canvas.height = $('.flex-item').height();
-    canvas.width = $('.flex-item').width();
-    let offset = parseInt(canvas.width) - parseInt(videow);
-    context.drawImage(video, offset, 0);
-    incomingfaceurl = canvas.toDataURL("image/jpg", 0.7);
-    socket.emit('incomingface', incomingfaceurl);
-    socket.emit('incomingemotion', incomingfaceurl);
-});
+}
 
 function getScore() {
     var poiTarget = [
@@ -102,34 +123,45 @@ function getScore() {
     else if (rolldiff > 6 && rolldiff <= 9) { score = score + 1 }
     else if (rolldiff > 9 && rolldiff <= 15) { score = score + 0.5 }
     console.log(`rolldiff: ${rolldiff}. Score so far: ${score}`);
-    if (yawdiff <= 3) { score = score + 2 }
-    else if (yawdiff > 3 && yawdiff <= 6) { score = score + 1.5 }
-    else if (yawdiff > 6 && yawdiff <= 9) { score = score + 1 }
-    else if (yawdiff > 9 && yawdiff <= 15) { score = score + 0.5 }
+    if (yawdiff <= 5) { score = score + 2 }
+    else if (yawdiff > 5 && yawdiff <= 10) { score = score + 1.5 }
+    else if (yawdiff > 10 && yawdiff <= 15) { score = score + 1 }
+    else if (yawdiff > 15 && yawdiff <= 20) { score = score + 0.5 }
 console.log(`yawdiff: ${yawdiff}. Score so far: ${score}`);
     if (happinessdiff <= .05) { score = score + 2 }
     else if (happinessdiff > .05 && happinessdiff <= .1) {score = score + 1.5}
     else if (happinessdiff > .1 && happinessdiff <= .15) {score = score + 1}
     else if (happinessdiff > .15 && happinessdiff < .2) {score = score + 0.5}
     console.log(`happinessdiff: ${happinessdiff}. Score so far: ${score}`);
-    if (neutraldiff <= .05) { score = score + 1 }
-    else if (neutraldiff > .05 && neutraldiff <= .1) {score = score + .75}
-    else if (neutraldiff > .1 && neutraldiff <= .15) {score = score + .5}
-    else if (neutraldiff > .15 && neutraldiff < .2) {score = score + .25} console.log(`neutraldiff: ${neutraldiff}. Score so far: ${score}`);
+    if (neutraldiff <= .05) { score = score + 2 }
+    else if (neutraldiff > .05 && neutraldiff <= .1) {score = score + 1.5}
+    else if (neutraldiff > .1 && neutraldiff <= .15) {score = score + 1}
+    else if (neutraldiff > .15 && neutraldiff < .2) {score = score + .5} console.log(`neutraldiff: ${neutraldiff}. Score so far: ${score}`);
 
-    score = Math.round(score / 7 * 100);
+    score = Math.round(score / 8 * 100);
 
     console.log(`Original: ${JSON.stringify(poiTarget)}. Incoming: ${JSON.stringify(poiIncoming)}`);
     console.log(`Score: ${score}`);
 
     if (score <= 45) { message = 'Not even close.' }
-    else if (score > 45 && score <= 65) { message = '*Squints* Uhh...' }
+    else if (score > 45 && score <= 65) { message = '*Squints* Hmm...' }
     else if (score > 65 && score <= 84) { message = 'Wow, nicely done!' }
     else if (score > 84 && score <= 95) { message = 'Amazing match!' }
     else if (score > 95) { message = 'Whoa, legit.' }
 
     document.getElementById('score').innerHTML = score + '%<p class="message">' + message + '</p>';
+    
+    $('.buttoncontainer').html('<button id="retry">Try another?</button>');
+    
+    score = 0;
+    yawdiff = 0;
+    rolldiff = 0;
+    neutraldiff = 0;
 
+    imgarray.push(imgarray.shift());
+    targetimgurl = imgarray[0];
+    socket.emit('targetface', targetimgurl);
+    socket.emit('targetemotion', targetimgurl);
 }
 
 function reloadPage() {
